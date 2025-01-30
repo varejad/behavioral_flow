fc05306d28165b77f1d5d15c079ed09d56e98d5a
varejad <junior18ademilson@gmail.com>
2025-01-30 16:08:38 -0300

    Biblioteca finalizada, falta finalizar o exemplo/teste

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 import random
 
 class Aprendente:
-
   # tem que validar se 'acoes' é um dicionario onde as chaves são tuplas e os valores são listas...
-  def __init__(self, acoes:dict, variar=False):
+  def __init__(self, acoes:dict, variar=False, prob_variacao=0.25):
     if not isinstance(acoes, dict):
       raise TypeError("O atributo 'acoes' deve ser uma dicionario onde as chaves são tuplas e os valores são listas...")
-
+    
+    self.prob_variacao = prob_variacao
     self.variar = variar
     self._acao_atual = ()
-    self._refocado = False
-    self._ultima_acao = []
-    self.respostas_padrao = acoes
-    self._antecedentes_e_respostas = {():self.respostas_padrao}
+    self._refocado = True
+    self._variou_na_ultima_tentativa = False
+    self._antecedentes_e_respostas = {():acoes}
     self.respostas_atuais = []
     self.antecedente_atual = []
 
   def proxima_acao(self, antecedente:list):
-    # validar se 'antecedente' é uma lista
     if not isinstance(antecedente, list):
       raise TypeError("O atributo 'antecedente' deve ser uma lista.")
     
-    # verifica se a ação atual não tem mais um passo a ser realizado, se tiver retorna a ação sem o passo já realizado
+    # sobre as ações geradas na variação: fica como resposabilidade de quem usar essa biblioteca garantir que 
+    # cada açao da lista de gerada na variação seja executada antes de pedir uma nova ação
+    """# verifica se a ação atual não tem mais um passo a ser realizado,
+    se tiver retorna a ação sem o passo já realizado
     if len(self._acao_atual) > 1:
-      self._acao_atual = self._acao_atual.pop(0)
-      return self._acao_atual
-    
-    # antes de definir a próxima ação, salva na variável _ultima_acao qual foi a ultima ação realizada
-    # self._ultima_acao = self._acao_atual
+      identado dentro desse if poderia estar o código para retornar a proxima acao da tupla até que fossem realizadas todas,
+      mas optei por outra abordagem descrita acima"""
 
     # define o antecedente atual como o parametro de antecedente recebido
     self.antecedente_atual = antecedente
@@ -38,13 +36,11 @@ class Aprendente:
     # verifica se foi reforçado, caso não, entra na função de variação
     if not self._refocado:
       variacao = self.variacao()
-      print(f"variação: {variacao}")
       if variacao is not None:
         self._acao_atual = variacao
         return self._acao_atual
 
     self._refocado = False
-
     self._acao_atual = self.definir_acao()
     return self._acao_atual
 
@@ -60,135 +56,26 @@ class Aprendente:
     if tuple(self.antecedente_atual) in self._antecedentes_e_respostas.keys():
       return self._antecedentes_e_respostas[tuple(self.antecedente_atual)]
     else:
-      return self.respostas_padrao
+      return dict(self._antecedentes_e_respostas[tuple()])
 
   def salvar_aprendizado(self):
     self._antecedentes_e_respostas[tuple(self.antecedente_atual)] = self.respostas_atuais
 
-  def reforcar(self, magnitude=1, acao=None):
+  def reforcar(self, magnitude=1, acao=tuple):
     self._refocado = True
-    if acao == None: acao = self._acao_atual
-    
+    if acao == tuple: acao = self._acao_atual #quando o parametro 'acao' não é passado
     self.respostas_atuais[acao][1] += magnitude
-
     self.salvar_aprendizado()
 
-  #do jeito que tá aqui vai variar 100% das vezes que for chamada essa função
-  #falta definir o custo e ver sobre a parte de salvar
   def variacao(self):
-    print("variação")
-    # verifica se a opção de variar está True
-    if self.variar:    
-      #retornar isso abaixo e ver como finalizar a função que chamou essa
-      #variacao = tuple(list(self._acao_atual) + list(self.definir_acao()))
-      #self._acao_atual = variacao  
-      return tuple(list(self._acao_atual) + list(self.definir_acao()))
-
-"""
-OKAY export var compAtual = {comp:[], fator: 0, custo: 0};
-var listaDeComp = [];
-OKAY export var i = 0;
-OKAY var reforcado = true;
-OKAY var ultimoComp;
-var antecesdentesERespostas = [{antecedente: null, resposta: null}]
-var antecedenteAtual
-var respostasPadrao
-var respostasAtuais
-var executarVariacao = {variar: false, fator: 0.85};
-
-export function novoComp(){
-  ultimoComp = compAtual;
-  resgatarAprendizado()
-  if( i < compAtual.comp.length - 1){
-    i++
-  }
-  else if (reforcado){
-    criaListaEDefinirComp();
-    //else if abaixo: extinção. se o comp existir dentro dos comp atuais (pode ser que não exista pq pode ser que tenha sido variação) entra no bloco
-  }
-  else if(respostasAtuais.findIndex(resposta => JSON.stringify(resposta) === JSON.stringify(compAtual)) !== -1){
-    if (executarVariacao.variar && compAtual.fator/listaDeComp.length > Math.random() + executarVariacao.fator){
-      variar()
-    }
-    else if (compAtual.fator > 4) {
-      reforcar(-1)
-    }
-    else{
-      criaListaEDefinirComp()}
-  }
-  else { 
-    criaListaEDefinirComp(); 
-  }
-
-  reforcado = false;
-  console.log(compAtual.comp + " " + compAtual.fator);
-  
-}
-
-function criaListaEDefinirComp(){
-  listaDeComp = []
-  for (const element of respostasAtuais){
-    let taxaDeProb = element.fator - element.custo;
-
-    for (let i = taxaDeProb; i > 0; i--){
-      listaDeComp.push(element);
-    }
-  }
-  i = 0;
-  atualizarCompAtual(JSON.stringify(listaDeComp[Math.floor(Math.random() * listaDeComp.length)]))  
-}
-
-export function variar(){
-  let listaParaEscolherVariacao = listaDeComp.filter(comp => comp.comp[0] !== "variar");
-  //a linha abaixo cria a variavel novoCompVariacao e define assim: seleciona um ítem aleatório da listaParaEscolherVariacao e transforma em string, depois pega a string e transforma em objeto e passa para a variavel criada
-  let novoCompVariacao = JSON.parse(JSON.stringify(listaParaEscolherVariacao[Math.floor(Math.random() * listaParaEscolherVariacao.length)]));
-
-  novoCompVariacao.comp = [...ultimoComp.comp, ...novoCompVariacao.comp];
-  novoCompVariacao.fator = Math.max(2, compAtual.fator * 0.7) 
-  novoCompVariacao.custo = compAtual.custo + novoCompVariacao.custo/2;
-
-  atualizarCompAtual(JSON.stringify(novoCompVariacao))
-  i++
-  console.log(compAtual.comp);
-
-  //algo dando errado com essa variação, especificamente no calculo do fator. (1)Não ta gerando um fator do tamanho esperado. (2)na lista imprime o fator de um tamanho e depois quando o comportamento variado é chamado imprime de outro jeito
-
-}
-
-export function reforcar(fatorDeIncremento = 3){
-  reforcado = true;
-  let index = respostasAtuais.findIndex(resposta => JSON.stringify(resposta.comp) === JSON.stringify(compAtual.comp))
-  if (index === -1){
-    respostasAtuais.push(compAtual);
-    index = respostasAtuais.length - 1;
-  }
-  respostasAtuais[index].fator += fatorDeIncremento
-}
-
-export function atualizarAntecedenteAtual(valor){
-  antecedenteAtual = valor;
-}
-
-export function resgatarAprendizado(){
-  let index = antecesdentesERespostas.findIndex(aprendizado => JSON.stringify(aprendizado.antecedente) === JSON.stringify(antecedenteAtual))
-  if (index === -1){ 
-    respostasAtuais = JSON.parse(JSON.stringify(respostasPadrao));
-    console.log(antecedenteAtual)
-    salvarAprendizado()
-  } else {
-    respostasAtuais = antecesdentesERespostas[index].resposta;
-  }
-}
-
-export function salvarAprendizado(){
-  let index = antecesdentesERespostas.findIndex(aprendizado => JSON.stringify(aprendizado.antecedente) === JSON.stringify(antecedenteAtual))
-  if (index === -1){ 
-    antecesdentesERespostas.push({antecedente: antecedenteAtual, resposta: respostasAtuais})
-  }
-}
-
-export function atualizarCompAtual(valor){
-  compAtual = JSON.parse(valor)
-}
-
-"""
+    if self._variou_na_ultima_tentativa == True:
+      self._variou_na_ultima_tentativa = False
+      return None
+    if self.variar and random.random() <= self.prob_variacao:
+      novo_passo = self.definir_acao() 
+      custo_da_variacao = self.respostas_atuais[self._acao_atual][0] + self.respostas_atuais[novo_passo][0]/2
+      fator_da_variacao = self.respostas_atuais[self._acao_atual][1]*0.75
+      #incluir nas respostas atuais, se for reforcado vai salvar, se não não salva (ESTA SALVANDO MESMO NÃO SENDO REFORÇADO)
+      self.respostas_atuais[tuple(list(self._acao_atual) + list(novo_passo))] = [custo_da_variacao,fator_da_variacao]
+      self._variou_na_ultima_tentativa = True
+      return tuple(list(self._acao_atual) + list(novo_passo))
--- teste.py
+++ teste.py
@@ -3,17 +3,32 @@ from main import Aprendente
 # define uma variável com um dicionário onde as chaves são tuplas contendo o "nome" da ação
 # e os valores são listas onde o indice [0] é o custo da resposta e o indice [1] é o fator da probabilidade
 # essa varia´vel será passada como atributo na instancia do apredente
-acoes_do_teste1 = {("andar_para_frente",): [2, 5],
-                   ("andar_para_tras",): [2, 5],
-                   ("ficar_parado",): [0, 5],
-                   ("pular",): [4, 500],
-                   ("andar_para_frente","andar_para_frente","andar_para_frente"): [2, 5]}
+acoes_do_teste1 = {("ação A",): [0, 5],
+                   ("ação B",): [0, 5],
+                   ("ação C",): [0, 5],
+                   ("ação D",): [0, 5],}
+
+# CRIAR AQUI A FUNÇÃO QUE PEDE PELA PROXIMA AÇÃO, EXECUTA A AÇÃO (INCLUSIVE SE ELA VIER COM MAIS DE UM PASSO) E PEDE NOVAMENTE PELA PROXIMA AÇÃO
 
 teste1 = Aprendente(acoes_do_teste1, variar=True)
-acao = teste1.proxima_acao(["nada"])
 
-print(f"teste: {acao}")
+for i in range(20):
+    print(teste1._antecedentes_e_respostas)
+    acao = teste1.proxima_acao(["nada"])
+    print(acao)
+    if acao == ("ação A",) or acao == ("ação B",):
+        #teste1.reforcar()
+        pass
+    if len(acao) == 2:
+        print(f"passos: {len(acao)}")
+        teste1.reforcar(-10)
+    print(len(teste1.respostas_atuais))
 
-if acao == ("pular",):
-    print("PULANDO")
-    teste1.reforcar(3)
+
+# sobre as ações geradas na variação: fica como resposabilidade de quem usar essa biblioteca garantir que 
+    # cada açao da lista de gerada na variação seja executada antes de pedir uma nova ação
+    """# verifica se a ação atual não tem mais um passo a ser realizado,
+    se tiver retorna a ação sem o passo já realizado
+    if len(self._acao_atual) > 1:
+      identado dentro desse if poderia estar o código para retornar a proxima acao da tupla até que fossem realizadas todas,
+      mas optei por outra abordagem descrita acima"""
