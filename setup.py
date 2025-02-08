3e0a70ba2b63f0a3c79cbcdfafc9a317b8f688a9
varejad <junior18ademilson@gmail.com>
2025-02-08 17:54:39 -0300

    preparando para publicar a lib

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ setup(
     author='Ademilson',
     author_email='junior18ademilson@gmail.com',
     description='Uma biblioteca para simular alguns principios comportamentais basicos, como reforçamento.',
-    long_description='Esta biblioteca...'
+    long_description='Esta biblioteca...',
     packages=find_packages(),
     install_requires=[],  # Dependências necessárias
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-    ]
+    ],
 )
